d8d80800ad5353dcba3558ec0b22cb18a0a8e3f6
anshuman <anshumanbajpai123@gmail.com>
2015-04-22 11:58:42 +0530

    try connection with door every 3 minutes

---
+++ src/to/talk/eternity/Config.java
@@ -0,0 +1,14 @@
+package to.talk.eternity;
+
+import java.net.Proxy;
+
+public class Config {
+
+    public static final String DOOR_HOST = "doormobile.handler.talk.to";
+    public static final int DOOR_PORT = 443;
+    public static final boolean USE_SECURE = true;
+    public static final int SOCKET_TIMEOUT = 6000;
+    public static final String REQUEST_STRING = "ping";
+    public static final Proxy PROXY = null;
+    public static final boolean INVALIDATE_SESSION = false;
+}
--- src/to/talk/eternity/MainActivity.java
+++ src/to/talk/eternity/MainActivity.java
@@ -1,6 +1,44 @@
 package to.talk.eternity;
 
 import android.app.Activity;
+import android.os.Bundle;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.SettableFuture;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
+
+    private final ScheduledExecutorService _executor = Executors.newSingleThreadScheduledExecutor();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        _executor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                testConnectivity();
+            }
+        }, 0, 3, TimeUnit.MINUTES);
+    }
+
+    private void testConnectivity() {
+
+        SettableFuture<ConnectionMetric> connectionFuture = NetworkClient.connect(Config.DOOR_HOST, Config.DOOR_PORT, Config.USE_SECURE, Config.SOCKET_TIMEOUT, Config.REQUEST_STRING, true, new ConnectionMetric(), Config.PROXY, Config.INVALIDATE_SESSION);
+        Futures.addCallback(connectionFuture, new FutureCallback<ConnectionMetric>() {
+            @Override
+            public void onSuccess(ConnectionMetric connectionMetric) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+
+
+            }
+        });
+    }
 }
