82ff1f80f6d92f62934a64fda79f25622c08eb97
Ayush Jindal <ayush.j@directi.com>
2015-04-30 10:05:14 +0530

    Add layout file

--- src/to/talk/eternity/MainActivity.java
+++ src/to/talk/eternity/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
         copyTcpDumpFile();
         _executor.scheduleAtFixedRate(new Runnable() {
             @Override
